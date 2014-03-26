136561fa6fca5316f046e54dd58bf1085444b799
Zhang Kai Yu <yeannylam@gmail.com>
2014-03-26 18:02:50 +0800

    Fixed a bug that cause the plugin not found

--- www/segue.js
+++ www/segue.js
@@ -1,6 +1,6 @@
 var segue = {
     performSegue: function (segueIdentifier) {
         var callback = function () {};
-        cordova.exec(callback, callback, "KYPushSeguePlugin", "performSegueWithUrlCommand", [segueIdentifier]);
+        cordova.exec(callback, callback, "segue", "performSegueWithUrlCommand", [segueIdentifier]);
     }
 };
